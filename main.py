414958a7711d0eeb51ac41bda3b3a88b1e145f73
plasma-games-shared <74255757+plasma-games-shared@users.noreply.github.com>
2021-01-04 15:05:21 -0500

    Just keep swimming

--- main.py
+++ main.py
@@ -5,28 +5,64 @@ from random import random
 
 csvpath = os.path.join('BoardgameData', 'BoardGameLibrary.csv')
 
-count = 0
+def Game_Picker(Players, PartyGame):
+    count = 0
 
-with open(csvpath, newline='') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
+    with open(csvpath, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',')
 
-    Header = next(csvreader, None)
+        Header = next(csvreader, None)
 
-    Name = []
-    Type = []
-    Min_player = []
-    Max_player = []
-    Play_time = []
+        NameList = []
+        TypeList = []
+        Min_playerList = []
+        Max_playerList = []
+        Play_timeList = []
 
-    for row in csvreader:
-        count += 1
-        Name.append(row[0])
-        Type.append(row[1])
-        Min_player.append(row[2])
-        Max_player.append(row[3])
-        Play_time.append(row[4])
-    
-    GameNum = int(random()*count-1) 
+        for row in csvreader:
+            count += 1
+            NameList.append(row[0])
+            TypeList.append(row[1])
+            Min_playerList.append(row[2])
+            Max_playerList.append(row[3])
+            Play_timeList.append(row[4])
+
+        GameNum = int(random()*count-1) 
+
+        GameName = NameList[GameNum]
+        GameType = TypeList[GameNum]
+        GameMinP = int(Min_playerList[GameNum])
+        GameMaxP = int(Max_playerList[GameNum])
+        GameTime = Play_timeList[GameNum]
+        Game = [GameName, GameType, GameMinP, GameMaxP, GameTime]
+        print(Game)
+
+        
+        return(Game)
+
+Players = int(input('How many players? > '))
+PartyGame = False
+if(input("Party Game? 'y' or 'n' > ")=='y'):
+    PartyGame = True
+print('Party:', PartyGame)
+
+Game = Game_Picker(Players, PartyGame)
+
+if('party' in Game and PartyGame == False):
+    Game = Game_Picker(Players, PartyGame)
+    print('1')
+elif('party' not in Game and PartyGame == True):
+    Game_Picker(Players, PartyGame)
+    print('2')
+elif(GameMinP > Players or GameMaxP < Players):
+    Game_Picker(Players, PartyGame)
+    print('3')
+
+print(Game_Picker(Players, PartyGame)[0])
+
+
+
+
 
     
 
